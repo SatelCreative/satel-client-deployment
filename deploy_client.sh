59f67c07b732f29d326253effa38d9d07e326901
Rahul <rahulpatidar0191@gmail.com>
2022-10-31 19:39:22 -0700

    remove js

--- deploy_client.sh
+++ deploy_client.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-APP_NAME=$1
-CLIENT=$2
+CLIENT=$1
+GEN_TYPE=$2
 
 # echo "APP_NAME=${APP_NAME}, CLIENT=${CLIENT}"
 
@@ -10,5 +10,9 @@ then
     cd $CLIENT
     yarn install
     yarn lint
+    if [[ $GEN_TYPE != None ]]
+    then
+        yarn gen 
+    fi   
     yarn build  
 fi  
