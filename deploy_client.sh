9c2ced65aa2f1974d1d85c8c227995e33244ba82
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2022-11-01 13:13:58 -0700

    remove debug statements

--- deploy_client.sh
+++ deploy_client.sh
@@ -2,17 +2,17 @@
 CLIENT=$1
 GEN_TYPE=$2
 
-echo "APP_NAME=${APP_NAME}, GEN_TYPE=${CLIENT}"
-echo "node version " node --version
+#echo "APP_NAME=${APP_NAME}, GEN_TYPE=${GEN_TYPE}"
+
 echo "Build client"
-# if [[ $CLIENT != None ]]
-# then
-#     cd $CLIENT
-#     yarn install
-#     yarn lint
-#     if [[ $GEN_TYPE != None ]]
-#     then
-#         yarn gen 
-#     fi   
-#     yarn build  
-# fi  
+if [[ $CLIENT != None ]]
+then
+    cd $CLIENT
+    yarn install
+    yarn lint
+    if [[ $GEN_TYPE != None ]]
+    then
+        yarn gen 
+    fi   
+    yarn build  
+fi  
